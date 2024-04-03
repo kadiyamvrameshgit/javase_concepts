197b07e34789d9f60f98ee884658bb0f0202c317
VenkatcoolK <132943034+VenkatcoolK@users.noreply.github.com>
2024-04-02 22:27:22 -0700

    java Addition commit two (#1)

---
+++ javase_practice/src/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+
+    public static void methodOne(int one , int two){
+
+        int totalCount = 0;
+
+        totalCount = one+two;
+        System.out.println("Addition of two numbers is =  " +totalCount);
+
+    }
+}
